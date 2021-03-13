5e7c2a8cb415a48f6b09fa1cde38439d5b66d909
ike ileke <kechileke444@gmail.com>
2021-03-13 05:29:06 +0000

    first commit

---
+++ try_it_yourself.py
@@ -0,0 +1,14 @@
+car = 'subaru'
+print("is car == 'subaru'? i predict true.")
+print(car == 'subaru')
+print("\n car == 'audi'? i predict false. ")
+print(car == 'audi')
+
+age = 45
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+print(f"your admission cost is ${price}")
